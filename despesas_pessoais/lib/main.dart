c472383921b074ae3a0e46603b8e8d0d8ca03d22
thiago <programacao.thiagogarcia@gmail.com>
2021-11-23 21:50:35 +0000

    Método para popular uma lista usando map

--- despesas_pessoais/lib/main.dart
+++ despesas_pessoais/lib/main.dart
@@ -1,17 +1,57 @@
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp( const MyApp());
-}
+import 'models/transaction.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    return  MaterialApp(
+      home: MyHomePage(),
     );
   }
+}
 
+class MyHomePage extends StatelessWidget {
+  final _transactions = [
+     Transaction(
+        id: 't1',
+        title: 'Novo tenis corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Conta de Luz',
+        value: 100.76,
+        date: DateTime.now()),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children:  [
+          const Card(
+            child: Text('Gráfico'),
+          ),
+          Column( // Modo de popula a coluna com os itens, lista de objetos virando componentes visuais
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
 }
